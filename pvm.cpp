68e1bfa2eff4d77f9bea25386040e8647a57afac
林久越 <43735056+HackLinjiuyue@users.noreply.github.com>
2019-03-13 18:43:44 +0800

    Update pvm.cpp
    add loop

--- pvm.cpp
+++ pvm.cpp
@@ -3,7 +3,6 @@
 #include<math.h>
 #include<vector>
 #include<string>
-#include<iostream>
 
 #define true 1
 #define false 0
@@ -33,8 +32,8 @@
 #define call 0x16
 #define call_method 0x17
 #define loop 0x18//先弹出代码块后条件
-#define make_block 0x19
-#define end_block 0x1a
+#define make_block_for_loop 0x19
+#define end_loop 0x1a
 #define make_func 0x1b//先弹出代码块后弹出参数个数
 #define push_fast 0x1c
 #define push_const 0x1d
@@ -43,21 +42,44 @@
 #define add_var 0x20
 #define push_array 0x21
 #define get_subscript 0x22
-#define iprint 0x23
-#define fprint 0x24
-#define sprint 0x25
-#define itof 0x26
-#define ftoi 0x27
-#define itos 0x28
-#define ftos 0x29
-#define pop 0x2a
-#define change_var 0x2b
+#define print 0x23
+#define itof 0x24
+#define ftoi 0x25
+#define itos 0x26
+#define ftos 0x27
+#define pop 0x28
+#define change_var 0x29//先弹出索引再弹出值
+
+#define beginblock_loop "0x19"
+#define endloop "0x1a"
 
 using namespace std;
 
 FILE *fp=NULL;
 
-wchar_t onget=0,type;
+wchar_t onget=0;
+
+int type;
+
+void delete_void(vector<void*> *stack,vector<int> *s_type,int index){
+	switch((*s_type)[index]){
+		case 0://str
+		delete((string*)*(stack->begin()+index));
+		break;
+		case 1://int
+		delete((int*)*(stack->begin()+index));
+		break;
+		case 2://double
+		delete((double*)*(stack->begin()+index));
+		break;
+	}
+}
+
+void Pop(vector<void*> *stack,vector<int> *s_type){
+	delete_void(stack,s_type,stack->size()-1);
+	s_type->pop_back();
+	stack->pop_back();
+}
 
 int wcount(wchar_t **s1){
 	int temp=0;
@@ -86,6 +108,15 @@ int htoi(string *s1,int start,int end){
 	return temp;
 }
 
+int getdec(double num){
+	int temp=0;
+	while((int)num!=num){
+		num*=10;
+		temp++;
+	}
+	return temp;
+}
+
 string get_from_file(int len){
 	int i=0;
 	string temp;
@@ -101,7 +132,23 @@ string get_from_file(int len){
 }
 
 
-wchar_t* get_from_code(string code,int start,int len);
+string get_from_code(string *code,int *start,int len){
+	string onstr="";
+	for(int i=0;i<len;i++){
+		onstr+=(*code)[*start];
+		(*start)++;
+	}
+	return onstr;
+};
+
+string getcode(int len,string *code,int *start,int isfile){
+	if(isfile){
+		return get_from_file(len);
+	}
+	else{
+		return get_from_code(code,start,len);
+	}
+}
 
 int getlen(wchar_t onstr,int isfile){
 	string str;
@@ -152,110 +199,74 @@ void* parse_const(string *s1){
 
 void Iadd(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)+*(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Isub(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)-*(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Imul(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)**(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Idiv(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)/ *(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Imod(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)-floor(*(int*)*(stack->end()-2)/ *(int*)stack->back())**(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Iequal(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)==*(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Igreater(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)>*(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Iless(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)<*(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fadd(vector<void*> *stack){
 	*(double*)*(stack->end()-2)=*(double*)*(stack->end()-2)+*(double*)stack->back();
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fsub(vector<void*> *stack){
 	*(double*)*(stack->end()-2)=*(double*)*(stack->end()-2)-*(double*)stack->back();
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fmul(vector<void*> *stack){
 	*(double*)*(stack->end()-2)=*(double*)*(stack->end()-2)**(double*)stack->back();
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fdiv(vector<void*> *stack){
 	*(double*)*(stack->end()-2)=*(double*)*(stack->end()-2)/ *(double*)stack->back();
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fmod(vector<void*> *stack){
 	*(double*)*(stack->end()-2)=*(double*)*(stack->end()-2)-floor(*(double*)*(stack->end()-2)/ *(double*)stack->back())**(double*)stack->back();
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fequal(vector<void*> *stack){
 	*(double*)*(stack->end()-2)=*(double*)*(stack->end()-2)== *(double*)stack->back();
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fgreater(vector<void*> *stack){
 	*(double*)*(stack->end()-2)=*(double*)*(stack->end()-2)>*(double*)stack->back();
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fless(vector<void*> *stack){
 	*(double*)*(stack->end()-2)=*(double*)*(stack->end()-2)<*(double*)stack->back();
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Sadd(vector<void*> *stack){
 	*(string*)*(stack->end()-2)=*(string*)*(stack->end()-2)+*(string*)stack->back();
-	delete((string*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Sequal(vector<void*> *stack){
 	*(string*)*(stack->end()-2)=*(string*)*(stack->end()-2)==*(string*)stack->back();
-	delete((string*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void NOT(vector<void*> *stack){
@@ -264,26 +275,18 @@ void NOT(vector<void*> *stack){
 
 void OR(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)||*(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void AND(vector<void*> *stack){
 	*(int*)*(stack->end()-2)=*(int*)*(stack->end()-2)&&*(int*)stack->back();
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Iprint(vector<void*> *stack){
 	printf("%i\n",*(int*)stack->back());
-	delete((int*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Fprint(vector<void*> *stack){
 	printf("%f\n",*(double*)stack->back());
-	delete((double*)*(stack->end()-1));
-	stack->pop_back();
 }
 
 void Sprint(vector<void*> *stack){
@@ -292,32 +295,112 @@ void Sprint(vector<void*> *stack){
 		printf("%c",(*onstr)[i]);
 	}
 	printf("\n" );
-	delete((string*)*(stack->end()-1));
-	stack->pop_back();
 }
 
-void Push_fast(vector<void*> *stack,int isfile){
-	string onstr;
-	if(isfile){
-		onstr=get_from_file(1);
+void Itof(vector<void*> *stack,vector<int> *s_type){
+	double *linshi=new double(*(int*)stack->back());
+	Pop(stack,s_type);
+	stack->push_back(linshi);
+	s_type->push_back(50);
+}
+
+void Ftoi(vector<void*> *stack,vector<int> *s_type){
+	int *linshi=new int(*(double*)stack->back());
+	Pop(stack,s_type);
+	stack->push_back(linshi);
+	s_type->push_back(49);
+}
+
+void Itos(vector<void*> *stack,vector<int> *s_type){
+	string *onstr=new string(to_string(*(int*)stack->back()));
+	Pop(stack,s_type);
+	stack->push_back(onstr);
+	s_type->push_back(48);
+};
+
+void Ftos(vector<void*> *stack,vector<int> *s_type){
+	double linshi=*(double*)stack->back();
+	string *onstr=new string(to_string(*(double*)stack->back()));
+	Pop(stack,s_type);
+	stack->push_back(onstr);
+	s_type->push_back(48);
+};
+
+void Loop(vector<void*> *stack,vector<int> *s_type,vector<string*> *loop_s,vector<int> *loop_p,string **onstr,int *on_loop,int *isfile,int *on_p){
+	int sign=*(int*)stack->back();
+	Pop(stack,s_type);
+	if(sign){
+		loop_s->push_back(*onstr);
+		loop_p->push_back(*on_p);
+		*onstr=(string*)stack->back();
+		stack->pop_back();
+		s_type->pop_back();
+		*isfile=0;
+		*on_p=0;
+		(*on_loop)++;
 	}
-	isfile=getlen(onstr[0],isfile);
+	else{
+		Pop(stack,s_type);
+	}
+}
+
+void Make_block_for_loop(vector<void*> *stack,vector<int> *s_type,int isfile,string *code,int *start){
+	string onstr,temp="";
+	int kh=1;
+	while(true){
+		onstr=getcode(4,code,start,isfile);
+		if(onstr==beginblock_loop){
+			kh++;
+		}
+		else if(onstr==endloop){
+			kh--;
+		}
+		if(kh==0){
+			temp+=endloop;
+			break;
+		}
+		temp+=onstr;
+	}
+	stack->push_back(new string(temp));
+	s_type->push_back(48);
+}
+
+void Push_fast(vector<void*> *stack,vector<int> *s_type,int isfile,string *code,int *start){
+	string onstr;
+	int on_p;
+	onstr=getcode(1,code,start,isfile);
+	on_p=getlen(onstr[0],isfile);
 	type=onstr[0];
-	onstr=get_from_file(isfile);
+	onstr=getcode(on_p,code,start,isfile);
 	stack->push_back(parse_const(&onstr));
+	s_type->push_back(type);
+}
+
+void Push_var(vector<void*> *stack,vector<void*> *var,vector<int> *s_type,vector<int> *v_type,int isfile,int *start,string *code){
+	Push_fast(stack,s_type,isfile,code,start);
+	int sign=*(int*)stack->back();
+	Pop(stack,s_type);
+	stack->push_back((*var)[sign]);
+	s_type->push_back((*v_type)[sign]);
+}
+
+void Change_var(vector<void*> *stack,vector<void*> *var,vector<int> *s_type,vector<int> *v_type){
+	int sign=*(int*)stack->back();
+	Pop(stack,s_type);
+	delete_void(var,v_type,sign);
+	*(var->begin()+sign)=stack->back();
+	*(v_type->begin()+sign)=s_type->back();
+	Pop(stack,s_type);
 }
 
 void vm(int isfile){
-	string code;
-	int sign=0;
+	string code,*onstr;
 	vector<void*> stack,pool,var;
+	vector<int> s_type,p_type,v_type,loop_p;
+	vector<string*> loop_s;
+	int on_loop=-1,on_p=0;
 	while(!feof(fp)){
-		if(isfile){
-			code=get_from_file(4);
-		}
-		else{
-			//code=get_from_code();
-		}
+		code=getcode(4,onstr,&on_p,isfile);
 		if(feof(fp)){
 			break;
 		}
@@ -326,109 +409,163 @@ void vm(int isfile){
 			break;
 			case iadd:
 			Iadd(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case isub:
 			Isub(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case imul:
 			Imul(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case idiv:
 			Idiv(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case imod:
 			Imod(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case fadd:
 			Fadd(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case fsub:
 			Fsub(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case fmul:
 			Fmul(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case fdiv:
 			Fdiv(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case fmod:
 			Fmod(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case sadd:
 			Sadd(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case iequal:
 			Iequal(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case fequal:
 			Fequal(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case sequal:
 			Sequal(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case Not:
 			NOT(&stack);
 			break;
 			case Or:
 			OR(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case And:
 			AND(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case i_greater:
 			Igreater(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case i_less:
 			Iless(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case f_greater:
 			Fgreater(&stack);
+			Pop(&stack,&s_type);
 			break;
 			case f_less:
 			Fless(&stack);
+			Pop(&stack,&s_type);
+			break;
+			case loop:
+			Loop(&stack,&s_type,&loop_s,&loop_p,&onstr,&on_loop,&isfile,&on_p);
+			break;
+			case make_block_for_loop:
+			if(on_loop!=-1){
+				Make_block_for_loop(&stack,&s_type,isfile,onstr,&on_p);
+			}
+			else{
+				Make_block_for_loop(&stack,&s_type,isfile,NULL,0);
+			}
+			break;
+			case end_loop:
+			if(*(int*)stack.back()){
+				on_p=0;
+			}
+			else{
+				delete(onstr);
+				onstr=loop_s.back();
+				loop_s.pop_back();
+				on_p=loop_p.back();
+				loop_p.pop_back();
+				on_loop--;
+			}
+			Pop(&stack,&s_type);
+			if(loop_s.size()==0){
+				isfile=1;
+			}
 			break;
 			case push_fast:
-			Push_fast(&stack,isfile);
+			Push_fast(&stack,&s_type,isfile,onstr,&on_p);
 			break;
 			case push_const:
-			Push_fast(&stack,isfile);
-			sign=*(int*)stack.back();
-			delete((int*)*(stack.end()-1));
-			stack.pop_back();
-			stack.push_back(pool[sign]);
+			Push_var(&stack,&pool,&s_type,&p_type,isfile,&on_p,onstr);
 			break;
 			case push_var:
-			Push_fast(&stack,isfile);
-			sign=*(int*)stack.back();
-			delete((int*)*(stack.end()-1));
-			stack.pop_back();
-			stack.push_back(var[sign]);
+			Push_var(&stack,&var,&s_type,&v_type,isfile,&on_p,onstr);
 			break;
 			case add_const:
-			Push_fast(&pool,isfile);
+			Push_fast(&pool,&s_type,isfile,onstr,&on_p);
 			break;
 			case add_var:
 			var.push_back(stack.back());
-			stack.pop_back();
+			v_type.push_back(s_type.back());
+			Pop(&stack,&s_type);
 			break;
-			case iprint:
-			Iprint(&stack);
+			case print:
+			switch(s_type.back()-'0'){
+				case 0:
+				Sprint(&stack);
+				break;
+				case 1:
+				Iprint(&stack);
+				break;
+				case 2:
+				Fprint(&stack);
+				break;
+			}
+			Pop(&stack,&s_type);
 			break;
-			case fprint:
-			Fprint(&stack);
+			case itof:
+			Itof(&stack,&s_type);
 			break;
-			case sprint:
-			Sprint(&stack);
+			case ftoi:
+			Ftoi(&stack,&s_type);
+			break;
+			case itos:
+			Itos(&stack,&s_type);
+			break;
+			case ftos:
+			Ftos(&stack,&s_type);
 			break;
 			case pop:
-			stack.pop_back();//待添加类型信息记录以及释放void*的内存
+			Pop(&stack,&s_type);
 			break;
 			case change_var:
-			sign=*(int*)stack.back();
-			delete((int*)*(stack.end()-1));
-			stack.pop_back();
-			var[sign]=stack.back();
-			stack.pop_back();
+			Change_var(&stack,&var,&s_type,&v_type);
 			break;
 		}
 	}
